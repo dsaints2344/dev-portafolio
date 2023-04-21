1a1ea513106115e773cc3dedbbaea53be3577340
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-04-21 00:51:40 -0400

    Adding layout to main page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import Layout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -58,7 +59,9 @@ export default function App({ Component, pageProps }: AppProps) {
         },
       }}
     >
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </MantineProvider>
   );
 }
