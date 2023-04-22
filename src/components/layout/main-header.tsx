fc88a8db1ba8421ccd94baf5adb4db4186c31c70
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-04-22 02:35:17 -0400

    Adding icon buttons

--- src/components/layout/main-header.tsx
+++ src/components/layout/main-header.tsx
@@ -4,24 +4,22 @@ import logo from "../../../public/logo.svg";
 import NavBar from "./nav-bar";
 const MainHeader: FC = () => {
   return (
-    <Container>
-      <Grid>
-        <Grid.Col span="auto">
-          <Image
-            w={227}
-            h={151}
-            fit="cover"
-            width="80%"
-            height="50%"
-            src={logo.src}
-            alt="header logo"
-          />
-        </Grid.Col>
-        <Grid.Col span="auto">
-          <NavBar />
-        </Grid.Col>
-      </Grid>
-    </Container>
+    <Grid>
+      <Grid.Col span={4}>
+        <Image
+          w={227}
+          h={151}
+          fit="contain"
+          width="75%"
+          height="50%"
+          src={logo.src}
+          alt="header logo"
+        />
+      </Grid.Col>
+      <Grid.Col span={8}>
+        <NavBar />
+      </Grid.Col>
+    </Grid>
   );
 };
 
--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -1,29 +1,44 @@
 import { FC } from "react";
-import { Flex, Button, Grid } from "@mantine/core";
+import { Flex, Button, Grid, ActionIcon, Box } from "@mantine/core";
+import { IconBrandLinkedin, IconBrandGithub } from "@tabler/icons-react";
 
 const NavBar: FC = () => {
   return (
-    <Grid>
-      <Grid.Col>
+    <Flex justify="space-around">
+      <Box m={5}>
         <Flex gap="lg" align="center" justify="flex-start" direction="row">
-          <Button variant="subtle" color="indigo.7">
+          <Button variant="subtle" color="indigo.7" size="lg">
             Home
           </Button>
-          <Button variant="subtle" color="indigo.7">
+          <Button variant="subtle" color="indigo.7" size="lg">
             About
           </Button>
-          <Button variant="subtle" color="indigo.7">
+          <Button variant="subtle" color="indigo.7" size="lg">
             Tech Stack
           </Button>
-          <Button variant="subtle" color="indigo.7">
+          <Button variant="subtle" color="indigo.7" size="lg">
             Projects
           </Button>
-          <Button variant="subtle" color="indigo.7">
+          <Button variant="subtle" color="indigo.7" size="lg">
             Contact
           </Button>
         </Flex>
-      </Grid.Col>
-    </Grid>
+      </Box>
+      <Box m={4}>
+        <Flex gap="lg" justify="flex-end" direction="row">
+          <Box mt={13}>
+            <ActionIcon color="indigo.7" variant="filled">
+              <IconBrandLinkedin />
+            </ActionIcon>
+          </Box>
+          <Box mt={13}>
+            <ActionIcon color="indigo.7" variant="filled">
+              <IconBrandGithub />
+            </ActionIcon>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
