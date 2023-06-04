9896091078da5a8d9195f6fd7ea68362fb3a960a
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-06-03 22:11:43 -0400

    Creating about me page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Text } from "@mantine/core";
-import classes from "../components/landing/header.module.css";
 import Welcome from "@/components/home/welcome";
 
 export default function Home() {
