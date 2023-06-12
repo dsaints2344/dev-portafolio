5252d045f703b6db867aa71cab071878f59a9837
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-06-11 20:40:25 -0400

    Testing calling supabase database

--- src/components/shared/timeline.tsx
+++ src/components/shared/timeline.tsx
@@ -1,6 +1,22 @@
-import { Box, Button, Flex, Title } from "@mantine/core";
+import { supabaseClient } from "@/api/supabase-client";
+import { IExperience } from "../../shared/types/experience";
+import { ActionIcon, Box, Button, Flex, Title, Text } from "@mantine/core";
+import { IconBuilding, IconCalendarStats } from "@tabler/icons-react";
+import { useCallback, useEffect, useState } from "react";
 
 const Timeline = () => {
+  const [experience, setData] = useState<IExperience[]>([]);
+
+  const loadExperience = useCallback(async () => {
+    let { data, error } = await supabaseClient.from("work_experience").select();
+
+    console.log(data);
+  }, []);
+
+  useEffect(() => {
+    loadExperience();
+  }, []);
+
   return (
     <Box w="50%">
       <Flex justify="space-between">
@@ -10,6 +26,24 @@ const Timeline = () => {
         <Button radius="xl" variant="light" color="green.9">
           Full Time
         </Button>
+      </Flex>
+      <Flex justify="space-between">
+        <Flex>
+          <ActionIcon>
+            <IconBuilding />
+          </ActionIcon>
+          <Box pt="0.5%">
+            <Text fs="xs">Novosit</Text>
+          </Box>
+        </Flex>
+        <Flex pt="0.5%">
+          <ActionIcon>
+            <IconCalendarStats />
+          </ActionIcon>
+          <Box pt="0.5%">
+            <Text fs="xs">Dec 2018 - Dec 2022</Text>
+          </Box>
+        </Flex>
       </Flex>
     </Box>
   );
