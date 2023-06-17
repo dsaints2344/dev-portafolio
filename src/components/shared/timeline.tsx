9399c5c24abe4edfba84808f8d62046574d6d86c
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-06-17 13:27:31 -0400

    displaying data from supabase client

--- src/components/shared/timeline.tsx
+++ src/components/shared/timeline.tsx
@@ -2,15 +2,15 @@ import { supabaseClient } from "@/api/supabase-client";
 import { IExperience } from "../../shared/types/experience";
 import { ActionIcon, Box, Button, Flex, Title, Text } from "@mantine/core";
 import { IconBuilding, IconCalendarStats } from "@tabler/icons-react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useFormatter } from "next-intl";
 
 const Timeline = () => {
   const [experience, setData] = useState<IExperience[]>([]);
-
+  const dateFormat = useFormatter();
   const loadExperience = useCallback(async () => {
     let { data, error } = await supabaseClient.from("work_experience").select();
-
-    console.log(data);
+    setData(data as IExperience[]);
   }, []);
 
   useEffect(() => {
@@ -18,34 +18,52 @@ const Timeline = () => {
   }, []);
 
   return (
-    <Box w="50%">
-      <Flex justify="space-between">
-        <Title color="indigo.8" size="h4" fw={400}>
-          Junior Web Developer
-        </Title>
-        <Button radius="xl" variant="light" color="green.9">
-          Full Time
-        </Button>
-      </Flex>
-      <Flex justify="space-between">
-        <Flex>
-          <ActionIcon>
-            <IconBuilding />
-          </ActionIcon>
-          <Box pt="0.5%">
-            <Text fs="xs">Novosit</Text>
+    <>
+      {experience.map((value) => {
+        return (
+          <Box w="50%" key={value.id}>
+            <Flex justify="space-between">
+              <Title color="indigo.8" size="h4" fw={400}>
+                {value.title}
+              </Title>
+              <Button radius="xl" variant="light" color="green.9">
+                {value.type}
+              </Button>
+            </Flex>
+            <Flex justify="space-between">
+              <Flex>
+                <ActionIcon>
+                  <IconBuilding />
+                </ActionIcon>
+                <Box pt="0.5%">
+                  <Text fs="xs">{value.location}</Text>
+                </Box>
+              </Flex>
+              <Flex pt="0.5%">
+                <ActionIcon>
+                  <IconCalendarStats />
+                </ActionIcon>
+                <Box pt="0.5%">
+                  <Text fs="xs">
+                    {dateFormat.dateTime(new Date(value.dateStart), {
+                      year: "numeric",
+                      month: "short",
+                    })}{" "}
+                    -{" "}
+                    {value.dateEnd
+                      ? dateFormat.dateTime(new Date(value.dateEnd), {
+                          year: "numeric",
+                          month: "short",
+                        })
+                      : "present"}
+                  </Text>
+                </Box>
+              </Flex>
+            </Flex>
           </Box>
-        </Flex>
-        <Flex pt="0.5%">
-          <ActionIcon>
-            <IconCalendarStats />
-          </ActionIcon>
-          <Box pt="0.5%">
-            <Text fs="xs">Dec 2018 - Dec 2022</Text>
-          </Box>
-        </Flex>
-      </Flex>
-    </Box>
+        );
+      })}
+    </>
   );
 };
 
